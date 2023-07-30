ce9cc4dc878f4ec3aa768f953cf93d7279ea1f2b
Ashraf Atef <ashraf.programer@gmail.com>
2023-07-30 17:49:48 +0300

    upload V1.0

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("test");
+let container = document.querySelector(".container");
+let circle = document.querySelector(".nav-circle");
+let openNav = document.querySelector(".nav-circle .open");
+let closeNav = document.querySelector(".nav-circle .close");
+let navList = document.querySelector("nav ul");
+
+console.log(closeNav);
+openNav.onclick = () => {
+  container.classList.add("show-nav");
+  circle.classList.add("active");
+  navList.classList.add("active");
+};
+closeNav.onclick = () => {
+  container.classList.remove("show-nav");
+  circle.classList.remove("active");
+  navList.classList.remove("active");
+};
